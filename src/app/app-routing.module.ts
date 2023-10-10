265be286f787810411d3a2ca37580d7eaa817ece
valentin-gerasin <gerasin1001@gmail.com>
2023-10-10 15:01:38 +0300

    init structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'chat',
+    loadChildren: () =>
+      import('./modules/chat/chat.module').then((m) => m.ChatModule),
+  },
+  {
+    path: 'settings',
+    loadComponent: () =>
+      import('./modules/settings/settings.module').then(
+        (m) => m.SettingsModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
