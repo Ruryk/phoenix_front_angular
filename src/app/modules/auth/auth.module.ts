96a8bb8f8d8317f746671a4f30445f056f4da06b
valentin-gerasin <gerasin1001@gmail.com>
2023-10-11 17:02:23 +0300

    feature: auth page

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './components/auth/auth.component';
 
 @NgModule({
   declarations: [AuthComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCheckboxModule,
+  ],
 })
 export class AuthModule {}
--- src/app/modules/auth/components/auth/auth.component.ts
+++ src/app/modules/auth/components/auth/auth.component.ts
@@ -1,7 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DestroyRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { passwordMatchValidator } from 'src/app/shared/utils/auth.functions';
 
 @Component({
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
 })
-export class AuthComponent {}
+export class AuthComponent implements OnInit {
+  public authRoutes: { signIn: string; signUp: string } = {
+    signIn: 'sign-in',
+    signUp: 'sign-up',
+  };
+
+  public signUpForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  public signInForm: FormGroup = this.fb.group(
+    {
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+    },
+    { validator: passwordMatchValidator('password', 'confirmPassword') }
+  );
+
+  public currentRoute$: Observable<string> = of(
+    this.activatedRoute.snapshot.routeConfig.path
+  );
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private destroyRef: DestroyRef,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.checkIsUserAuthorized();
+
+    this.currentRoute$
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((data) => data);
+  }
+
+  public signUp() {
+    this.authService.signUp({
+      email: this.signUpForm.controls['email'].value,
+      password: this.signUpForm.controls['password'].value,
+    });
+  }
+
+  public signIn() {
+    this.authService.signIn({
+      email: this.signInForm.controls['email'].value,
+      password: this.signInForm.controls['password'].value,
+    });
+  }
+
+  public navigateTo(path: string): void {
+    this.router.navigate([`/auth/${path}`]);
+  }
+
+  public checkIsUserAuthorized(): void {
+    this.authService.isUserAuthorized()
+      ? this.router.navigate([`/chat`])
+      : false;
+  }
+}
